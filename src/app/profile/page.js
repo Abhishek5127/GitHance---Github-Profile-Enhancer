6a95f9f5d30d9aeacef9e340505b08698190ac64
Abhishek Choudhary <itzabhishek5127@gmail.com>
2025-12-27 13:15:39 +0530

    connecting api calls to frontend

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,35 +1,51 @@
+"use client"
+import { useState,useEffect } from "react"
 import UserDataBlock from "./profile-components/userDataBlock"
 import UserReposBlock from "./profile-components/userReposBlock"
 
-export default function profile() {
-    const [userData, setUserData] = useState([{}])
-    const [username, setUsername] = useState("")
+export default function Profile() {
+    const [userData, setUserData] = useState(null)
+    const [username, setUsername] = useState("abhishek5127")
+    
 
-    const getUserData = async() => {
+    useEffect(()=>{
         
-        try {
-            if (!username) {
-                response.json("Username not provided")
-            }
-        } catch (error) {
-            console.error("username", error);
-        }
+        const getUserData = async() => {
         
-        try {
-            
-            const data = await fetch("/api/github",
-                   username
-               )
-           
+  try {
 
-        } catch (error) {
-
-        }
-
-
+    if (!username) {
+      console.error("Username not provided");
+      return;
     }
 
+    const res = await fetch("/api/github", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username }),
+    });
 
+    if (!res.ok) {
+      throw new Error("Failed to fetch user data");
+    }
+
+    const data =  await res.json();
+    console.log("User data:", data);
+    setUserData(data)
+
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+  }
+};  
+
+  getUserData();
+
+
+    },[username])
+
+    
     return (
         <div>
             <UserDataBlock />
