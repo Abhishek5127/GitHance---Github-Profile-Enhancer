5207a217641fc743189c7aa4075786b43df9d458
Abhishek Choudhary <itzabhishek5127@gmail.com>
2026-03-23 00:45:57 +0530

    readme 1 finished

--- src/app/UI/home/FeatureGridElements/readme1.jsx
+++ src/app/UI/home/FeatureGridElements/readme1.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 import { assets } from '@/app/assets/assets'
+import { bannerAssets } from './BannerAssets/bannerAssets'
 import SquareTechStack from '../FeatureGridReusables/techStacks/squareTechStack'
 
 const Readme1 = () => {
   return (
-    <div className='w-full'>
+    <div className=' w-full'>
 
       <h1 className='text-3xl font-serif bg-green-600 p-2 w-full text-center'>
         John Doe
@@ -28,9 +29,34 @@ const Readme1 = () => {
         <div>
           <SquareTechStack/>
         </div>
-        
       </div>
+      <div>
+        <div className='text-[14px] underline'>Streak</div>
+        <div className='flex m-2 gap-3'>
+        <Image src={bannerAssets.Streak}
+        height={40}
+        width={100}
+        alt='streak'
+        className='rounded-[5px]'/>
+
+         <Image src={bannerAssets.Contribution}
+        height={40}
+        width={100}
+        alt='streak'
+        className='rounded-[5px]'/>
+        </div>
+      </div>
+      <footer className='h-20 w-[100%] relative'>
+        <div className='flex justify-center w-full items-center absolute text-black top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+          <h3>Nothing Beats Consistancy</h3>
+        </div>
+        <Image
+        src={assets.BannerImg1}
+        alt='footer'
+        className="w-full h-20 object-cover"/>
+      </footer>
           </div>
+          
     </div>
   )
 }
