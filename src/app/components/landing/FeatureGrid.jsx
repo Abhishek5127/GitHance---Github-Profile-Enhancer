97c28691a6d5bdc0daf2fa4e5d448742ed7d925b
Abhishek Choudhary <itzabhishek5127@gmail.com>
2026-03-24 01:09:52 +0530

    right side card

--- src/app/components/landing/FeatureGrid.jsx
+++ src/app/components/landing/FeatureGrid.jsx
@@ -19,7 +19,7 @@ export default function FeatureGrid() {
     const slider = sliderRef.current;
 
     function animate() {
-      angle.current -=0.70;
+      angle.current -= 0.70;
       slider.style.setProperty("--rotate", angle.current + "deg");
       requestAnimationFrame(animate);
     }
@@ -62,8 +62,24 @@ export default function FeatureGrid() {
     Readme8,
   ];
 
+  const FeatureCard = [
+    {
+      feature: "Auto-Updating Images",
+      description: "Contribution graphs and Streaks update with each commit and time intervals."
+    },
+    {
+      feature: "Visually Appealing Craft",
+      description: "Customized Eye Catching readme components to turn boring data into masterpiece."
+    },
+    {
+      feature: "AI Powered Genration",
+      description: "Transforms your codebase into clear, structured documentation automatically."
+    }
+  ]
+
   return (
-    <section id="solutions" className="feature-grid-section">
+    <section id="solutions" className="feature-grid-section flex justify-between">
+
       <div className="fg-wrapper" style={{ "--quantity": components.length }}>
         <div className="fg-slider" ref={sliderRef}>
           {components.map((Comp, i) => (
@@ -78,6 +94,34 @@ export default function FeatureGrid() {
             </div>
           ))}
         </div>
+      </div>
+
+
+      <div className="h-full w-[60%] flex flex-col justify-center items-center text-center h-full bg-[#0D0D0D]">
+        <div className="flex bg-blue-500 text-[#FF7A1A] justify-center items-center">
+          <h1 className="font-antonio text-9xl">4X</h1>
+          <h1 className="text-7xl font-danfo">{'>'}</h1>
+          <h1 className="text-7xl font-danfo">{'>'}</h1>
+          <h1 className="text-7xl font-danfo">{'>'}</h1>
+        </div>
+        <div>
+          <div className="flex mt-4 gap-3">
+
+            {FeatureCard.map((item) => (
+              <div className="" key={item.feature}>
+                <div className="bg-[#131313] h-[150px] w-[210px] pt-3">
+                  <h1 className="text-[16px] font-bold">{item.feature}</h1>
+                  <div className="mt-1 ml-5">
+
+                  <p className="text-gray-500 text-left text-[12px] p-1">{item.description}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+
       </div>
     </section>
   );
